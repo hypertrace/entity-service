9c73335edfe1b93dad7779afe8cf6734cf5675f3
Mohit Agarwal <mohit@traceable.ai>
2020-07-24 14:22:08 +0530

    Update to 0.1.3

--- entity-service-client/build.gradle.kts
+++ entity-service-client/build.gradle.kts
@@ -12,7 +12,7 @@ dependencies {
   api(project(":entity-service-api"))
   api("com.typesafe:config:1.4.0")
 
-  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.1.2")
+  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.1.3")
   implementation("org.slf4j:slf4j-api:1.7.30")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
