13651e0d4c24a002e4b5916e12fe9ad6b145bb90
Aaron Steinfeld <aaron@traceable.ai>
2021-04-02 10:56:49 -0400

    feat: add async entity write to rx client

--- entity-data-service-rx-client/src/main/java/org/hypertrace/entity/data/service/rxclient/EntityDataClient.java
+++ entity-data-service-rx-client/src/main/java/org/hypertrace/entity/data/service/rxclient/EntityDataClient.java
@@ -3,16 +3,18 @@ package org.hypertrace.entity.data.service.rxclient;
 import io.grpc.CallCredentials;
 import io.grpc.Channel;
 import io.reactivex.rxjava3.core.Single;
+import java.time.Clock;
 import java.time.Duration;
 import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 import javax.annotation.Nonnull;
 import org.hypertrace.core.grpcutils.client.RequestContextClientCallCredsProviderFactory;
 import org.hypertrace.entity.data.service.v1.Entity;
+import org.hypertrace.entity.data.service.v1.MergeAndUpsertEntityRequest.UpsertCondition;
 
 /**
  * EntityDataClient is an asynchronous, potentially caching client, keyin entities as described in
- * {@link EntityCacheKey}
+ * {@link EntityKey}
  */
 public interface EntityDataClient {
 
@@ -23,13 +25,42 @@ public interface EntityDataClient {
    * @param entity
    * @return
    */
+  @Deprecated
   Single<Entity> getOrCreateEntity(Entity entity);
+
+  /**
+   * Performs a throttled update of the provided entity, starting after no longer than the provided
+   * maximumUpsertDelay. If newer candidates for the same entity are received before this update
+   * occurs, the value of the newest value (and its condition, if any) will be used instead. This
+   * allows a high number of potentially repetitive entity upserts to be processed without creating
+   * excessive overhead. Each returned single will propagate the result of the server call that
+   * eventually satisfies its deadline.
+   *
+   * @param entity
+   * @param upsertCondition
+   * @param maximumUpsertDelay
+   * @return
+   */
+  Single<Entity> createOrUpdateEntityEventually(
+      Entity entity, UpsertCondition upsertCondition, Duration maximumUpsertDelay);
+
+  /**
+   * Immediately creates or updates, merging with any existing data, the provided entity if the
+   * provided condition is met. The new value is returned if the condition is met, else if the
+   * condition is not met, the existing value is instead returned.
+   *
+   * @param entity
+   * @param upsertCondition
+   * @return The resulting entity
+   */
+  Single<Entity> createOrUpdateEntity(Entity entity, UpsertCondition upsertCondition);
 
   static Builder builder(@Nonnull Channel channel) {
     return new Builder(Objects.requireNonNull(channel));
   }
 
   final class Builder {
+    private final Clock clock = Clock.systemUTC();
     private final Channel channel;
     private int maxCacheContexts = 1000;
     private Duration cacheExpiration = Duration.of(15, ChronoUnit.MINUTES);
@@ -42,7 +73,11 @@ public interface EntityDataClient {
 
     public EntityDataClient build() {
       return new EntityDataCachingClient(
-          this.channel, this.callCredentials, this.maxCacheContexts, this.cacheExpiration);
+          this.clock,
+          this.channel,
+          this.callCredentials,
+          this.maxCacheContexts,
+          this.cacheExpiration);
     }
 
     /**
