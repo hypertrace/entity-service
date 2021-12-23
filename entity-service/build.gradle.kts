e70e465c33a48a17e99e1bf3dc6e38423abc8ad5
ravisingal <62086374+ravisingal@users.noreply.github.com>
2021-12-23 21:54:15 +0530

    upgrade log4j version to 2.17.0 (#153)

--- entity-service/build.gradle.kts
+++ entity-service/build.gradle.kts
@@ -29,7 +29,7 @@ dependencies {
 
   // Logging
   implementation("org.slf4j:slf4j-api:1.7.30")
-  runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.16.0")
+  runtimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.17.0")
 
   // Config
   implementation("com.typesafe:config:1.4.1")
