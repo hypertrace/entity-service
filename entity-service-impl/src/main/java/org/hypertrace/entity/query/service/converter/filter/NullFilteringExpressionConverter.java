e7dee70c6a045d30870f518bc9c41108fcc8f045
SJ <shubham@traceable.ai>
2022-09-15 12:21:57 +0530

    use identifier expression converter

--- entity-service-impl/src/main/java/org/hypertrace/entity/query/service/converter/filter/NullFilteringExpressionConverter.java
+++ entity-service-impl/src/main/java/org/hypertrace/entity/query/service/converter/filter/NullFilteringExpressionConverter.java
@@ -1,9 +1,5 @@
 package org.hypertrace.entity.query.service.converter.filter;
 
-import static org.hypertrace.entity.query.service.converter.ValueHelper.VALUES_KEY;
-import static org.hypertrace.entity.query.service.converter.ValueHelper.VALUE_LIST_KEY;
-import static org.hypertrace.entity.query.service.converter.identifier.IdentifierConverter.getSubDocPathById;
-
 import com.google.inject.Inject;
 import lombok.AllArgsConstructor;
 import org.hypertrace.core.documentstore.expression.impl.ConstantExpression;
@@ -12,18 +8,16 @@ import org.hypertrace.core.documentstore.expression.impl.RelationalExpression;
 import org.hypertrace.core.documentstore.expression.operators.RelationalOperator;
 import org.hypertrace.core.documentstore.expression.type.FilterTypeExpression;
 import org.hypertrace.core.grpcutils.context.RequestContext;
-import org.hypertrace.entity.attribute.translator.EntityAttributeMapping;
 import org.hypertrace.entity.query.service.converter.ConversionException;
 import org.hypertrace.entity.query.service.converter.Converter;
+import org.hypertrace.entity.query.service.converter.identifier.IdentifierExpressionConverter;
 import org.hypertrace.entity.query.service.v1.ColumnIdentifier;
 import org.hypertrace.entity.query.service.v1.LiteralConstant;
 import org.hypertrace.entity.query.service.v1.Operator;
 
 @AllArgsConstructor(onConstructor_ = {@Inject})
 public class NullFilteringExpressionConverter extends FilteringExpressionConverterBase {
-  private static final String ARRAY_SUFFIX = "." + VALUE_LIST_KEY + "." + VALUES_KEY;
-
-  private final EntityAttributeMapping entityAttributeMapping;
+  private final IdentifierExpressionConverter identifierExpressionConverter;
   private final Converter<LiteralConstant, ConstantExpression> constantExpressionConverter;
 
   @Override
@@ -33,10 +27,9 @@ public class NullFilteringExpressionConverter extends FilteringExpressionConvert
       final LiteralConstant constant,
       final RequestContext requestContext)
       throws ConversionException {
-    final String columnId = columnIdentifier.getColumnName();
-    final String subDocPath = getSubDocPath(columnId, requestContext);
+    final IdentifierExpression identifierExpression =
+        identifierExpressionConverter.convert(columnIdentifier, requestContext);
 
-    final IdentifierExpression identifierExpression = IdentifierExpression.of(subDocPath);
     final RelationalOperator relationalOperator = convertOperator(operator);
     final ConstantExpression constantExpression =
         constantExpressionConverter.convert(constant, requestContext);
@@ -44,6 +37,15 @@ public class NullFilteringExpressionConverter extends FilteringExpressionConvert
     RelationalExpression relationalExpression =
         RelationalExpression.of(identifierExpression, relationalOperator, constantExpression);
 
+    /**
+     * An array is stored as 'attributes.array_field.valueList.values.%d.value.&lt;type&gt;'.
+     *
+     * <p>Example: products = ['shoes', 'socks'] is stored as
+     * 'attributes.names.valueList.values.0.value.string: shoes', and
+     * 'attributes.names.valueList.values.1.value.string: socks'.
+     *
+     * <p>An empty array is stored as 'attributes.array_field.valueList: {}'
+     */
     switch (operator) {
       case EQ:
         // 'field' EQ 'null' -> 'field' NOT_EXISTS
@@ -60,24 +62,5 @@ public class NullFilteringExpressionConverter extends FilteringExpressionConvert
       default:
         return relationalExpression;
     }
-  }
-
-  /**
-   * An array is stored as 'attributes.array_field.valueList.values.%d.value.&lt;type&gt;'.
-   *
-   * <p>Example: products = ['shoes', 'socks'] is stored as
-   * 'attributes.names.valueList.values.0.value.string: shoes', and
-   * 'attributes.names.valueList.values.1.value.string: socks'.
-   *
-   * <p>An empty array is stored as 'attributes.array_field.valueList: {}'
-   */
-  private String getSubDocPath(String columnId, RequestContext requestContext)
-      throws ConversionException {
-    String subDocPath = getSubDocPathById(entityAttributeMapping, columnId, requestContext);
-    return !isArrayField(columnId, requestContext) ? subDocPath : subDocPath + ARRAY_SUFFIX;
-  }
-
-  private boolean isArrayField(final String columnId, final RequestContext requestContext) {
-    return entityAttributeMapping.isMultiValued(requestContext, columnId);
   }
 }
--- entity-service-impl/src/test/java/org/hypertrace/entity/query/service/converter/filter/NullFilteringExpressionConverterTest.java
+++ entity-service-impl/src/test/java/org/hypertrace/entity/query/service/converter/filter/NullFilteringExpressionConverterTest.java
@@ -3,16 +3,15 @@ package org.hypertrace.entity.query.service.converter.filter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 
-import java.util.Optional;
 import org.hypertrace.core.documentstore.expression.impl.ConstantExpression;
 import org.hypertrace.core.documentstore.expression.impl.IdentifierExpression;
 import org.hypertrace.core.documentstore.expression.impl.RelationalExpression;
 import org.hypertrace.core.documentstore.expression.operators.RelationalOperator;
 import org.hypertrace.core.documentstore.expression.type.FilterTypeExpression;
 import org.hypertrace.core.grpcutils.context.RequestContext;
-import org.hypertrace.entity.attribute.translator.EntityAttributeMapping;
 import org.hypertrace.entity.query.service.converter.ConversionException;
 import org.hypertrace.entity.query.service.converter.Converter;
+import org.hypertrace.entity.query.service.converter.identifier.IdentifierExpressionConverter;
 import org.hypertrace.entity.query.service.v1.ColumnIdentifier;
 import org.hypertrace.entity.query.service.v1.LiteralConstant;
 import org.hypertrace.entity.query.service.v1.Operator;
@@ -27,7 +26,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 class NullFilteringExpressionConverterTest {
   private static final RequestContext REQUEST_CONTEXT = RequestContext.forTenantId("tenant1");
 
-  @Mock private EntityAttributeMapping entityAttributeMapping;
+  @Mock private IdentifierExpressionConverter identifierExpressionConverter;
   @Mock private Converter<LiteralConstant, ConstantExpression> constantExpressionConverter;
 
   private NullFilteringExpressionConverter nullFilteringExpressionConverter;
@@ -35,7 +34,8 @@ class NullFilteringExpressionConverterTest {
   @BeforeEach
   void setup() {
     nullFilteringExpressionConverter =
-        new NullFilteringExpressionConverter(entityAttributeMapping, constantExpressionConverter);
+        new NullFilteringExpressionConverter(
+            identifierExpressionConverter, constantExpressionConverter);
   }
 
   @Test
@@ -45,9 +45,9 @@ class NullFilteringExpressionConverterTest {
     LiteralConstant constant =
         LiteralConstant.newBuilder().setValue(Value.newBuilder().setString("null").build()).build();
 
-    when(entityAttributeMapping.getDocStorePathByAttributeId(REQUEST_CONTEXT, "column1"))
-        .thenReturn(Optional.of("attributes.subDocPath1"));
-    when(entityAttributeMapping.isMultiValued(REQUEST_CONTEXT, "column1")).thenReturn(false);
+    IdentifierExpression identifierExpression = IdentifierExpression.of("attributes.subDocPath1");
+    when(identifierExpressionConverter.convert(columnIdentifier, REQUEST_CONTEXT))
+        .thenReturn(identifierExpression);
 
     ConstantExpression constantExpression = ConstantExpression.of("null");
     when(constantExpressionConverter.convert(constant, REQUEST_CONTEXT))
@@ -56,7 +56,6 @@ class NullFilteringExpressionConverterTest {
     FilterTypeExpression filterTypeExpression =
         nullFilteringExpressionConverter.convert(
             columnIdentifier, Operator.EQ, constant, REQUEST_CONTEXT);
-    IdentifierExpression identifierExpression = IdentifierExpression.of("attributes.subDocPath1");
 
     assertEquals(
         RelationalExpression.of(
@@ -71,62 +70,9 @@ class NullFilteringExpressionConverterTest {
     LiteralConstant constant =
         LiteralConstant.newBuilder().setValue(Value.newBuilder().setString("null").build()).build();
 
-    when(entityAttributeMapping.getDocStorePathByAttributeId(REQUEST_CONTEXT, "column1"))
-        .thenReturn(Optional.of("attributes.subDocPath1"));
-    when(entityAttributeMapping.isMultiValued(REQUEST_CONTEXT, "column1")).thenReturn(false);
-
-    ConstantExpression constantExpression = ConstantExpression.of("null");
-    when(constantExpressionConverter.convert(constant, REQUEST_CONTEXT))
-        .thenReturn(constantExpression);
-
-    FilterTypeExpression filterTypeExpression =
-        nullFilteringExpressionConverter.convert(
-            columnIdentifier, Operator.NEQ, constant, REQUEST_CONTEXT);
     IdentifierExpression identifierExpression = IdentifierExpression.of("attributes.subDocPath1");
-
-    assertEquals(
-        RelationalExpression.of(
-            identifierExpression, RelationalOperator.EXISTS, constantExpression),
-        filterTypeExpression);
-  }
-
-  @Test
-  void testArrayEqNull() throws ConversionException {
-    ColumnIdentifier columnIdentifier =
-        ColumnIdentifier.newBuilder().setColumnName("column1").build();
-    LiteralConstant constant =
-        LiteralConstant.newBuilder().setValue(Value.newBuilder().setString("null").build()).build();
-
-    when(entityAttributeMapping.getDocStorePathByAttributeId(REQUEST_CONTEXT, "column1"))
-        .thenReturn(Optional.of("attributes.subDocPath1"));
-    when(entityAttributeMapping.isMultiValued(REQUEST_CONTEXT, "column1")).thenReturn(true);
-
-    ConstantExpression constantExpression = ConstantExpression.of("null");
-    when(constantExpressionConverter.convert(constant, REQUEST_CONTEXT))
-        .thenReturn(constantExpression);
-
-    FilterTypeExpression filterTypeExpression =
-        nullFilteringExpressionConverter.convert(
-            columnIdentifier, Operator.EQ, constant, REQUEST_CONTEXT);
-    IdentifierExpression identifierExpression =
-        IdentifierExpression.of("attributes.subDocPath1.valueList.values");
-
-    assertEquals(
-        RelationalExpression.of(
-            identifierExpression, RelationalOperator.NOT_EXISTS, constantExpression),
-        filterTypeExpression);
-  }
-
-  @Test
-  void testArrayNeqNull() throws ConversionException {
-    ColumnIdentifier columnIdentifier =
-        ColumnIdentifier.newBuilder().setColumnName("column1").build();
-    LiteralConstant constant =
-        LiteralConstant.newBuilder().setValue(Value.newBuilder().setString("null").build()).build();
-
-    when(entityAttributeMapping.getDocStorePathByAttributeId(REQUEST_CONTEXT, "column1"))
-        .thenReturn(Optional.of("attributes.subDocPath1"));
-    when(entityAttributeMapping.isMultiValued(REQUEST_CONTEXT, "column1")).thenReturn(true);
+    when(identifierExpressionConverter.convert(columnIdentifier, REQUEST_CONTEXT))
+        .thenReturn(identifierExpression);
 
     ConstantExpression constantExpression = ConstantExpression.of("null");
     when(constantExpressionConverter.convert(constant, REQUEST_CONTEXT))
@@ -135,8 +81,6 @@ class NullFilteringExpressionConverterTest {
     FilterTypeExpression filterTypeExpression =
         nullFilteringExpressionConverter.convert(
             columnIdentifier, Operator.NEQ, constant, REQUEST_CONTEXT);
-    IdentifierExpression identifierExpression =
-        IdentifierExpression.of("attributes.subDocPath1.valueList.values");
 
     assertEquals(
         RelationalExpression.of(
