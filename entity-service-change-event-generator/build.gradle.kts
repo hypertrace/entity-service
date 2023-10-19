72beaee94a069c105a613adf9bd3b6d9a42f9ca9
aman bansal <bansalaman2905@gmail.com>
2023-10-19 12:13:11 +0530

    fix | fix vulnerability (#255)
    * fix vulnerability

--- entity-service-change-event-generator/build.gradle.kts
+++ entity-service-change-event-generator/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
   api("com.typesafe:config:1.4.1")
 
   implementation("org.hypertrace.core.eventstore:event-store:0.1.2")
-  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.12.5")
+  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.12.6")
   implementation("com.google.guava:guava:32.1.2-jre")
   implementation("org.slf4j:slf4j-api:1.7.30")
 
@@ -28,7 +28,7 @@ dependencies {
   }
 
   runtimeOnly("io.confluent:kafka-protobuf-serializer")
-  implementation(platform("org.hypertrace.core.kafkastreams.framework:kafka-bom:0.3.7"))
+  implementation(platform("org.hypertrace.core.kafkastreams.framework:kafka-bom:0.4.2"))
 
   annotationProcessor("org.projectlombok:lombok:1.18.18")
   compileOnly("org.projectlombok:lombok:1.18.18")
