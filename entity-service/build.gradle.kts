7db28c6332c758f0ee61697a22e41ebb7fc64c44
Bhuvnesh Bamoriya <48723434+bhubam@users.noreply.github.com>
2023-10-27 15:22:57 +0530

    update version of document store (#258)

--- entity-service/build.gradle.kts
+++ entity-service/build.gradle.kts
@@ -23,7 +23,7 @@ dependencies {
   implementation(project(":entity-service-factory"))
 
   implementation("org.hypertrace.core.serviceframework:platform-grpc-service-framework:0.1.62")
-  implementation("org.hypertrace.core.documentstore:document-store:0.7.46")
+  implementation("org.hypertrace.core.documentstore:document-store:0.7.47")
 
   runtimeOnly("io.grpc:grpc-netty:1.57.2")
 
