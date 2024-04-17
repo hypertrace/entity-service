704f9fb7c2892e9f45bdc125cd6da9a4f16dd138
Dhamodhar <56181018+Dhamodhar-DDR@users.noreply.github.com>
2024-04-17 16:39:40 +0530

    updated service framework version (#279)
    * updated service framework version
    
    * update grpc and jackson versions to match hypertrace-bom
    
    * updated grpc-utils version to match hypertrace-bom
    
    ---------
    
    Co-authored-by: Dhamodhar Reddy  Dakannagari <dhamodhar@Dhamodhars-MacBook-Pro.local>

--- entity-data-service-rx-client/build.gradle.kts
+++ entity-data-service-rx-client/build.gradle.kts
@@ -13,8 +13,8 @@ dependencies {
   api(project(":entity-service-api"))
   api("io.reactivex.rxjava3:rxjava:3.0.11")
 
-  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.12.6")
-  implementation("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.12.6")
+  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.13.1")
+  implementation("org.hypertrace.core.grpcutils:grpc-client-rx-utils:0.13.1")
 
   implementation("org.slf4j:slf4j-api:1.7.30")
   implementation("com.google.guava:guava:32.1.2-jre")
@@ -24,6 +24,6 @@ dependencies {
   testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
   testImplementation("org.mockito:mockito-core:3.8.0")
   testImplementation("org.mockito:mockito-junit-jupiter:3.8.0")
-  testImplementation("io.grpc:grpc-core:1.57.2")
+  testImplementation("io.grpc:grpc-core:1.60.0")
   testRuntimeOnly("org.apache.logging.log4j:log4j-slf4j-impl:2.17.1")
 }
--- entity-service-impl/build.gradle.kts
+++ entity-service-impl/build.gradle.kts
@@ -7,17 +7,17 @@ plugins {
 dependencies {
   api(project(":entity-service-api"))
   api(project(":entity-service-attribute-translator"))
-  api("org.hypertrace.core.serviceframework:service-framework-spi:0.1.62")
+  api("org.hypertrace.core.serviceframework:service-framework-spi:0.1.71")
 
   annotationProcessor("org.projectlombok:lombok:1.18.22")
   compileOnly("org.projectlombok:lombok:1.18.18")
 
   implementation("org.hypertrace.core.documentstore:document-store:0.7.58")
-  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.12.6")
-  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.12.6")
+  implementation("org.hypertrace.core.grpcutils:grpc-context-utils:0.13.1")
+  implementation("org.hypertrace.core.grpcutils:grpc-client-utils:0.13.1")
   implementation("org.hypertrace.core.attribute.service:caching-attribute-service-client:0.14.15")
-  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.62")
-  implementation("org.hypertrace.core.serviceframework:docstore-metrics:0.1.62")
+  implementation("org.hypertrace.core.serviceframework:platform-metrics:0.1.71")
+  implementation("org.hypertrace.core.serviceframework:docstore-metrics:0.1.71")
   implementation("com.google.inject:guice:5.0.1")
 
   implementation(project(":entity-type-service-rx-client"))
@@ -29,7 +29,7 @@ dependencies {
   implementation("com.google.guava:guava:32.1.2-jre")
 
   implementation("org.slf4j:slf4j-api:1.7.30")
-  implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+  implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
 
   testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
   testImplementation("org.mockito:mockito-core:3.8.0")
